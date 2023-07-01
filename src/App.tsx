3bd0dd5dbe0a5e1e19e925a73e00b870b4965265
naka520 <yashipipi0520@gmail.com>
2023-07-01 13:07:24 +0900

    "deployエラー　解消　警告オフ"

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import logo from "./logo.svg";
+// import React from "react";
+// import logo from "./logo.svg";
 import "./App.css";
 import Signup from "./signup";
 import Header from "./header";
