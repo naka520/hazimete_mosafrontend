2d6910427748486dd3f305cbd402d436cd4ddee8
naka520 <yashipipi0520@gmail.com>
2023-07-01 18:04:55 +0900

    "deployエラー修正　コメントアウト④"

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./header";
+
 import AdministratorLogin from "./Administrator/Login";
 import AdministratorSignup from "./Administrator/Signup";
 
