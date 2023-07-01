9fa84b8c6eb6e8dd9d96fff7838780adf8175450
naka520 <yashipipi0520@gmail.com>
2023-07-01 17:42:29 +0900

    "ログイン画面の配置"

--- src/Administrator/Login.tsx
+++ src/Administrator/Login.tsx
@@ -21,6 +21,23 @@ const App: React.FC = () => {
           backgroundColor: "blue", // 適宜背景色を変更
         }}
       />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh", // 画面の縦幅いっぱいに表示する場合
+          width: "100vw", // 画面の横幅いっぱいに表示する場合
+        }}
+      >
+        <Box
+          sx={{
+            width: "300px", // 適宜サイズを調整
+            height: "200px", // 適宜サイズを調整
+            backgroundColor: "blue", // 適宜背景色を変更
+          }}
+        />
+      </Box>
     </Box>
   );
 };
