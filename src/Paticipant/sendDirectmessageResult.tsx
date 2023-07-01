f23f7b56e79c19302a11693e039286157a7c0762
Tanakaryuki <tnk3257.rt@gmail.com>
2023-07-02 02:40:45 +0900

    DM送信完了画面が完成

--- src/Paticipant/sendDirectmessageResult.tsx
+++ src/Paticipant/sendDirectmessageResult.tsx
@@ -1,9 +1,23 @@
-// import { Link } from "react-router-dom";
 import Header from "./../header";
+import Box from '@mui/material/Box';
+import SubHeader from "./../subheader";
+
 function SendDirectmessageResult() {
   return (
     <div>
       <Header />
+      <SubHeader />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '30vh',
+          fontSize: '4rem', // フォントサイズを4remに設定
+        }}
+      >
+        <h1>DM送信完了</h1>
+      </Box>
     </div>
   );
 }
