8cd21d68adea3e9b0c6e39ba0042cce70eeec863
naka520 <yashipipi0520@gmail.com>
2023-07-01 17:42:29 +0900

    "ログイン画面の配置"

--- src/Administrator/Login.tsx
+++ src/Administrator/Login.tsx
@@ -1,7 +1,27 @@
 // import { Link } from "react-router-dom";
+import React from "react";
+import { Box } from "@mui/material";
 
-function AdministratorLogin() {
-  return <div></div>;
-}
+const App: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh", // 画面の縦幅いっぱいに表示する場合
+        width: "100vw", // 画面の横幅いっぱいに表示する場合
+      }}
+    >
+      <Box
+        sx={{
+          width: "300px", // 適宜サイズを調整
+          height: "200px", // 適宜サイズを調整
+          backgroundColor: "blue", // 適宜背景色を変更
+        }}
+      />
+    </Box>
+  );
+};
 
-export default AdministratorLogin;
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route
