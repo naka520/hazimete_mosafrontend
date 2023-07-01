c0c86187e8e48c3b51bfd238a92c307ce1714dae
naka520 <yashipipi0520@gmail.com>
2023-07-01 16:46:04 +0900

    "deployエラー解消　コメントアウト"

--- src/Administrator/Signup.tsx
+++ src/Administrator/Signup.tsx
@@ -1,18 +1,18 @@
-import { ThemeProvider } from "@mui/material";
-import { Theme } from "@mui/material/styles";
-import { createTheme } from "@mui/material/styles";
+// import { ThemeProvider } from "@mui/material";
+// import { Theme } from "@mui/material/styles";
+// import { createTheme } from "@mui/material/styles";
 
 // カラーテーマの定義
-const theme: Theme = createTheme({
-  palette: {
-    primary: {
-      main: "#FF0000", // プライマリカラーを赤に設定
-    },
-    secondary: {
-      main: "#06C756", // セカンダリカラーを緑に設定
-    },
-  },
-});
+// const theme: Theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: "#FF0000", // プライマリカラーを赤に設定
+//     },
+//     secondary: {
+//       main: "#06C756", // セカンダリカラーを緑に設定
+//     },
+//   },
+// });
 
 function AdministratorSignup(): any {
   return (
