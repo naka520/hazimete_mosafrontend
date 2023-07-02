c5fd0c5506e8568bda1184fb5cf8d6398a7f5aad
Tanakaryuki <tnk3257.rt@gmail.com>
2023-07-02 13:43:08 +0900

    Board完成

--- src/Administrator/Board.tsx
+++ src/Administrator/Board.tsx
@@ -2,12 +2,216 @@
 //BoardはroleListのこと
 import Header from "./../header";
 import SubHeader from "./../subheader";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import EmailIcon from '@mui/icons-material/Email';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { DataGrid, GridColDef} from '@mui/x-data-grid';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+// import { Link } from "react-router-dom";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    border: {
+      main: string;
+    };
+  }
+  interface PaletteOptions {
+    border?: {
+      main?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#06C756", // プライマリカラーを赤に設定
+    },
+    secondary: {
+      main: "#DDDDDD", // セカンダリカラーを緑に設定
+    },
+    border: {
+      main: "#DDDDDD", // セカンダリカラーを緑に設定
+    },
+  },
+});
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 600,
+  height: 300,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+const columns: GridColDef[] = [
+  { field: 'display_name', headerName: '名前', width: 170 },
+  { field: 'id', headerName: 'username', width: 130 },
+];
+
+const rows = [
+  { display_name: '田中',id:'Ryuki'},
+  { display_name: '田中',id:'tanaka'},
+  { display_name: '田中',id:'Sato'},
+  { display_name: '田中',id:'Hayashi'},
+  { display_name: '田中',id:'Neko'},
+  { display_name: '田中',id:'God'},
+];
 
 function Board() {
+  const [openOne, setOpenOne] = React.useState(false);
+  const [openTwo, setOpenTwo] = React.useState(false);
+
+  const handleOpenOne = () => setOpenOne(true);
+  const handleCloseOne = () => setOpenOne(false);
+
+  const handleOpenTwo = () => setOpenTwo(true);
+  const handleCloseTwo = () => setOpenTwo(false);
   return (
-    <div>
+    <div style={{ height: 400, width: '100%' }}>
       <Header />
       <SubHeader title="体育祭"/>
+      <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="xl">
+        <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "10vh",
+        }}
+      ></Box>
+      <Stack direction="row" spacing={2}>
+      <ThemeProvider theme={theme}>
+      <Button onClick={handleOpenOne} variant="outlined" startIcon={<AddIcon />}>
+        ロール作成
+      </Button>
+      <Modal
+        open={openOne}
+        onClose={handleCloseOne}
+        aria-labelledby="modal-modal-title-1"
+        aria-describedby="modal-modal-description-1"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            ロール作成
+          </Typography>
+          <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "1vh",
+        }}
+      ></Box >
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <TextField
+          id="outlined-multiline-static"
+          label="メッセージ"
+          multiline
+          rows={4}
+          defaultValue=""
+          sx={{ width: '70%', height: '100%' }}
+        />
+    </Box>
+    <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "3vh",
+        }}
+      ></Box>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+    <Button variant="contained" sx={{ color: '#FFFFFF' }}>出席確認</Button>
+    </Box>
+        </Box>
+      </Modal>
+      <Button onClick={handleOpenTwo} variant="outlined" startIcon={<EmailIcon />}>
+        メッセージ送信
+      </Button>
+      <Modal
+        open={openTwo}
+        onClose={handleCloseTwo}
+        aria-labelledby="modal-modal-title-2"
+        aria-describedby="modal-modal-description-2"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            メッセージ送信
+          </Typography>
+          <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "1vh",
+        }}
+      ></Box >
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <TextField
+          id="outlined-multiline-static"
+          label="メッセージ"
+          multiline
+          rows={4}
+          defaultValue=""
+          sx={{ width: '70%', height: '100%' }}
+        />
+    </Box>
+    <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "3vh",
+        }}
+      ></Box>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+    <Button variant="contained" sx={{ color: '#FFFFFF' }}>メッセージ送信</Button>
+    </Box>
+        </Box>
+      </Modal>
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        削除
+      </Button>
+      </ThemeProvider>
+    </Stack>
+    <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "1vh",
+        }}
+      ></Box>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+      </Container>
+    </React.Fragment>
     </div>
   );
 }
