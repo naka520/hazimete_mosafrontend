4b862136997a3edc00cc3655464bd100538fd8af
Tanakaryuki <tnk3257.rt@gmail.com>
2023-07-05 21:01:37 +0900

    Board画面を修正

--- src/Administrator/Board.tsx
+++ src/Administrator/Board.tsx
@@ -2,21 +2,25 @@
 //BoardはroleListのこと
 import Header from "./../header";
 import SubHeader from "./../subheader";
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import AddIcon from '@mui/icons-material/Add';
-import EmailIcon from '@mui/icons-material/Email';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import { DataGrid, GridColDef} from '@mui/x-data-grid';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import EmailIcon from "@mui/icons-material/Email";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 // import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+//import axios from "axios";
+import { useEffect } from "react";
+import Chip from '@mui/material/Chip';
 
 declare module "@mui/material/styles" {
   interface Palette {
@@ -46,172 +50,314 @@ const theme = createTheme({
 });
 
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 600,
   height: 300,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 const columns: GridColDef[] = [
-  { field: 'display_name', headerName: '名前', width: 170 },
-  { field: 'id', headerName: 'username', width: 130 },
+  { field: "id", headerName: "ロール名", width: 170 },
+  { field: "members", headerName: "人数", width: 130 },
 ];
 
 const rows = [
-  { display_name: '田中',id:'Ryuki'},
-  { display_name: '田中',id:'tanaka'},
-  { display_name: '田中',id:'Sato'},
-  { display_name: '田中',id:'Hayashi'},
-  { display_name: '田中',id:'Neko'},
-  { display_name: '田中',id:'God'},
+  { id: "体育祭", members: "4" },
+  { id: "文化祭", members: "2" },
+  { id: "入学式", members: "7" },
+  { id: "卒業式", members: "29" },
+  { id: "飲み会", members: "2" }
 ];
+
+interface UserData {
+  user_uuid: string;
+  username: string;
+  display_name: string;
+  line_user: {
+    line_user_uuid: string;
+    user_id: string;
+  };
+}
 
 function Board() {
   const [openOne, setOpenOne] = React.useState(false);
   const [openTwo, setOpenTwo] = React.useState(false);
+  const [openThree, setOpenThree] = React.useState(false);
 
   const handleOpenOne = () => setOpenOne(true);
   const handleCloseOne = () => setOpenOne(false);
 
   const handleOpenTwo = () => setOpenTwo(true);
   const handleCloseTwo = () => setOpenTwo(false);
+
+  const handleOpenThree = () => setOpenThree(true);
+  const handleCloseThree = () => setOpenThree(false);
+
+  const navigate = useNavigate();
+  // useEffect(() => {
+  //   const fetchUserData = async () => {
+  //     try {
+  //       const endpoint = "https://mosa-cup-backend.azurewebsites.net/api/v1/me";
+  //       const response = await fetch(endpoint);
+  //       console.log(response);
+  //       const userData: UserData = await response.json();
+  //       console.log(userData);
+  //       if (response.status == 401) {
+  //         navigate("/Administrator/Login");
+  //       }
+  //     } catch (error) {
+  //       console.error("APIの呼び出し中にエラーが発生しました。", error);
+  //     }
+  //   };
+
+  //   fetchUserData();
+  // }, [navigate]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [userData, setUserData] = React.useState<UserData | null>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const endpoint = "https://mosa-cup-backend.azurewebsites.net/api/v1/me";
+        const response = await fetch(endpoint);
+        const userData: UserData = await response.json();
+        setUserData(userData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("APIの呼び出し中にエラーが発生しました。", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!userData) {
+    navigate("/Administrator/Login");
+    return null;
+  }
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 400, width: "100%" }}>
       <Header />
-      <SubHeader title="体育祭"/>
+      <SubHeader title="体育祭" />
       <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="xl">
-        <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "10vh",
-        }}
-      ></Box>
-      <Stack direction="row" spacing={2}>
-      <ThemeProvider theme={theme}>
-      <Button onClick={handleOpenOne} variant="outlined" startIcon={<AddIcon />}>
-        ロール作成
-      </Button>
-      <Modal
-        open={openOne}
-        onClose={handleCloseOne}
-        aria-labelledby="modal-modal-title-1"
-        aria-describedby="modal-modal-description-1"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            ロール作成
-          </Typography>
+        <CssBaseline />
+        <Container maxWidth="xl">
           <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "1vh",
-        }}
-      ></Box >
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <TextField
-          id="outlined-multiline-static"
-          label="メッセージ"
-          multiline
-          rows={4}
-          defaultValue=""
-          sx={{ width: '70%', height: '100%' }}
-        />
-    </Box>
-    <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "3vh",
-        }}
-      ></Box>
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-    <Button variant="contained" sx={{ color: '#FFFFFF' }}>出席確認</Button>
-    </Box>
-        </Box>
-      </Modal>
-      <Button onClick={handleOpenTwo} variant="outlined" startIcon={<EmailIcon />}>
-        メッセージ送信
-      </Button>
-      <Modal
-        open={openTwo}
-        onClose={handleCloseTwo}
-        aria-labelledby="modal-modal-title-2"
-        aria-describedby="modal-modal-description-2"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            メッセージ送信
-          </Typography>
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "10vh",
+            }}
+          ></Box>
+          <Stack direction="row" spacing={2}>
+            <ThemeProvider theme={theme}>
+              <Button
+                onClick={handleOpenOne}
+                variant="outlined"
+                startIcon={<AddIcon />}
+              >
+                ロール作成
+              </Button>
+              <Modal
+                open={openOne}
+                onClose={handleCloseOne}
+                aria-labelledby="modal-modal-title-1"
+                aria-describedby="modal-modal-description-1"
+              >
+                <Box sx={style}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    ロール作成
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      minHeight: "4vh",
+                    }}
+                  ></Box>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <TextField id="outlined-basic" label="ロール名" variant="outlined" />
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      minHeight: "5vh",
+                    }}
+                  ></Box>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Button variant="contained" sx={{ color: "#FFFFFF" }}>
+                      作成
+                    </Button>
+                  </Box>
+                </Box>
+              </Modal>
+              <Button
+                onClick={handleOpenTwo}
+                variant="outlined"
+                startIcon={<EmailIcon />}
+              >
+                メッセージ送信
+              </Button>
+              <Modal
+                open={openTwo}
+                onClose={handleCloseTwo}
+                aria-labelledby="modal-modal-title-2"
+                aria-describedby="modal-modal-description-2"
+              >
+                <Box sx={style}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    メッセージ送信
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      minHeight: "2vh",
+                    }}
+                  ></Box>
+                  <Stack direction="row" spacing={1}>
+                    <p>メッセージ送信先：</p>
+                    <Chip label="体育祭" variant="outlined" />
+                    <Chip label="文化祭" variant="outlined" />
+                  </Stack>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      minHeight: "1.5vh",
+                    }}
+                  ></Box>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <TextField
+                      id="outlined-multiline-static"
+                      label="メッセージ"
+                      multiline
+                      rows={3}
+                      defaultValue=""
+                      sx={{ width: "70%", height: "100%" }}
+                    />
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      minHeight: "2vh",
+                    }}
+                  ></Box>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Button variant="contained" sx={{ color: "#FFFFFF" }}>
+                      メッセージ送信
+                    </Button>
+                  </Box>
+                </Box>
+              </Modal>
+              <Button
+                onClick={handleOpenThree}
+                variant="outlined"
+                startIcon={<DeleteIcon />}
+              >
+                削除
+              </Button>
+              <Modal
+                open={openThree}
+                onClose={handleCloseThree}
+                aria-labelledby="modal-modal-title-1"
+                aria-describedby="modal-modal-description-1"
+              >
+                <Box sx={style}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    ロール削除
+                  </Typography>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      minHeight: "2vh",
+                    }}
+                  ></Box>
+                  <p>以下のロールを削除して良いですか？</p>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      minHeight: "3vh",
+                    }}
+                  ></Box>
+                  <Stack direction="row" spacing={1}>
+                    <p>削除するロール：</p>
+                    <Chip label="体育祭" variant="outlined" />
+                    <Chip label="文化祭" variant="outlined" />
+                  </Stack>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      minHeight: "6vh",
+                    }}
+                  ></Box>
+                  <Box sx={{ display: "flex", justifyContent: "center" }}>
+                    <Button variant="contained" sx={{ color: "#FFFFFF" }}>
+                      削除
+                    </Button>
+                  </Box>
+                </Box>
+              </Modal>
+            </ThemeProvider>
+          </Stack>
           <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "1vh",
-        }}
-      ></Box >
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-          <TextField
-          id="outlined-multiline-static"
-          label="メッセージ"
-          multiline
-          rows={4}
-          defaultValue=""
-          sx={{ width: '70%', height: '100%' }}
-        />
-    </Box>
-    <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "3vh",
-        }}
-      ></Box>
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-    <Button variant="contained" sx={{ color: '#FFFFFF' }}>メッセージ送信</Button>
-    </Box>
-        </Box>
-      </Modal>
-      <Button variant="outlined" startIcon={<DeleteIcon />}>
-        削除
-      </Button>
-      </ThemeProvider>
-    </Stack>
-    <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "1vh",
-        }}
-      ></Box>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
-      </Container>
-    </React.Fragment>
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "1vh",
+            }}
+          ></Box>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
+            }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+          />
+        </Container>
+      </React.Fragment>
     </div>
   );
 }
