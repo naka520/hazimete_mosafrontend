d55faf49bae8ff86612514afada8dcadc2f010f3
Tanakaryuki <tnk3257.rt@gmail.com>
2023-07-07 12:34:09 +0900

    ログイン状態でヘッダーを変化するように

--- src/header.tsx
+++ src/header.tsx
@@ -7,6 +7,8 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 // import IconButton from "@mui/material/IconButton";
 // import MenuIcon from "@mui/icons-material/Menu";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 declare module "@mui/material/styles" {
   interface Palette {
@@ -36,6 +38,44 @@ const theme = createTheme({
 });
 
 export default function ButtonAppBar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [userData, setUserData] = useState(null);
+  const [displayName, setDisplayName] = useState("");
+  const [firstLevelPath, setFirstLevelPath] = useState("");
+
+  const endpointUrl = "https://mosa-cup-backend.azurewebsites.net/api/v1/me";
+
+  useEffect(() => {
+    // ローカルストレージからaccess_tokenを取得する
+    const accessToken = localStorage.getItem("access_token");
+
+    // access_tokenが存在する場合はログイン済みとみなす
+    setIsLoggedIn(accessToken !== null);
+
+    if (accessToken) {
+      // ログイン済みの場合、APIサーバにGETリクエストを送信
+      axios
+        .get(endpointUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((response) => {
+          // APIからのレスポンスを受け取り、ユーザーデータをセット
+          setUserData(response.data);
+          setDisplayName(response.data.display_name);
+        })
+        .catch((error) => {
+          // エラーハンドリング
+          console.error("APIリクエストエラー:", error);
+        });
+    }
+    const path = window.location.pathname;
+    const firstLevel = path.split("/")[1];
+    setFirstLevelPath(firstLevel);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Box>
@@ -50,14 +90,26 @@ export default function ButtonAppBar() {
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               WebBoard
             </Typography>
-            <Box
-              sx={{ "& > :not(style) + :not(style)": { marginLeft: "8px" } }}
-            >
-              <Button variant="contained" color="secondary">
-                <Typography color="primary"><a href="/Administrator/Login">ログイン</a></Typography>
-              </Button>
-              <Button variant="contained"><a href="/Administrator/Signup">アカウント作成</a></Button>
-            </Box>
+            {isLoggedIn ? (
+              <p>{displayName}さん</p>
+            ) : (
+              <p>
+                <Box
+                  sx={{
+                    "& > :not(style) + :not(style)": { marginLeft: "8px" },
+                  }}
+                >
+                  <Button variant="contained" color="secondary">
+                    <Typography color="primary">
+                      <a href={`/${firstLevelPath}/Login`}>ログイン</a>
+                    </Typography>
+                  </Button>
+                  <Button variant="contained">
+                    <a href={`/${firstLevelPath}/Signup`}>アカウント作成</a>
+                  </Button>
+                </Box>
+              </p>
+            )}
           </Toolbar>
         </AppBar>
       </Box>
