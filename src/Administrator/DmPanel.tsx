339467e1659047212856d409361ac3b0f7882d7f
naka520 <yashipipi0520@gmail.com>
2023-08-11 22:40:44 +0900

    [modify]UIの変更

--- src/Administrator/DmPanel.tsx
+++ src/Administrator/DmPanel.tsx
@@ -1,90 +1,199 @@
-// DmDetail.tsx
+// // DmDetail.tsx
+
+// import React from "react";
+// // import { useParams } from "react-router-dom";
+// import { Card, CardContent, Typography } from "@mui/material";
+// // import dmData from "./Dmdata";
+// // import { useState } from "react";
+// // import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+// // import mockDmList from "./mockData";
+
+// import {
+//   Container,
+//   Box,
+// } from "@mui/material";
+// import Header from "./../header";
+// import SubHeader from "./../subheader";
+
+
+
+// interface Message {
+//   id: number;
+//   content: string;
+// }
+// // const theme = createTheme({
+// //   palette: {
+// //     primary: {
+// //       main: "#06C756", // プライマリカラーを赤に設定
+// //     },
+// //     secondary: {
+// //       main: "#DDDDDD", // セカンダリカラーを緑に設定
+// //     },
+// //     border: {
+// //       main: "#DDDDDD", // セカンダリカラーを緑に設定
+// //     },
+// //   },
+// // });
+
+
+
+// const DmPanel: React.FC = () => {
+//   // const { dmId } = useParams<{ dmId: any }>();
+//   const dm = {
+//       id: 1,
+//       sender: "田中",
+//       content: "選手交代のお願い",
+//       timestamp: "2023-08-06T12:00:00",
+//     };
+
+//   if (!dm) {
+//     return <div>DMが見つかりませんでした。</div>;
+//   }
+
+//   return (
+//     <div>
+//       <div style={{ height: 400, width: "100%" }}>
+//       <Header />
+//       <SubHeader title="体育祭" />
+//     <Container maxWidth="sm">
+//     <Box
+//             sx={{
+//               display: "flex",
+//               justifyContent: "center",
+//               alignItems: "center",
+//               minHeight: "10vh",
+//             }}
+//           ></Box>
+//       <Box sx={{ width: "100%", typography: "body1" }}>
+//       <Box sx={{ width: "100%", typography: "body1" }}></Box>
+//       <Card>
+//         <CardContent>
+//           <Typography variant="h6">
+//             送信者: {dm.sender}
+//           </Typography>
+//           <Typography variant="body1">
+//             内容: {dm.content}
+//           </Typography>
+//           <Typography variant="body2" color="textSecondary">
+//             タイムスタンプ: {dm.timestamp}
+//           </Typography>
+//         </CardContent>
+//       </Card>
+//       </Box>
+//     </Container>
+//     </div>
+//     </div>
+//   );
+// };
+
+// export default DmPanel;
 
 import React from "react";
-// import { useParams } from "react-router-dom";
-import { Card, CardContent, Typography } from "@mui/material";
-// import dmData from "./Dmdata";
-// import { useState } from "react";
-// import { ThemeProvider, createTheme } from "@mui/material/styles";
-
-// import mockDmList from "./mockData";
-
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import {
+  Card,
+  CardContent,
+  Typography,
   Container,
   Box,
+  List,
+  ListItem,
 } from "@mui/material";
 import Header from "./../header";
 import SubHeader from "./../subheader";
-
-
+import dmData from "./Dmdata";
 
 interface Message {
   id: number;
+  sender: string;
   content: string;
+  timestamp: string;
 }
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: "#06C756", // プライマリカラーを赤に設定
-//     },
-//     secondary: {
-//       main: "#DDDDDD", // セカンダリカラーを緑に設定
-//     },
-//     border: {
-//       main: "#DDDDDD", // セカンダリカラーを緑に設定
-//     },
-//   },
-// });
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#06C756", // プライマリカラーを赤に設定
+    },
+    secondary: {
+      main: "#DDDDDD", // セカンダリカラーを緑に設定
+    },
+    border: {
+      main: "#DDDDDD", // セカンダリカラーを緑に設定
+    },
+  },
+});
 
 
+// ...（import文など）
 
 const DmPanel: React.FC = () => {
-  // const { dmId } = useParams<{ dmId: any }>();
-  const dm = {
-      id: 1,
-      sender: "田中",
-      content: "選手交代のお願い",
-      timestamp: "2023-08-06T12:00:00",
-    };
-
-  if (!dm) {
-    return <div>DMが見つかりませんでした。</div>;
-  }
+  const messages: Message[] = Object.values(dmData);
 
   return (
     <div>
-      <div style={{ height: 400, width: "100%" }}>
       <Header />
       <SubHeader title="体育祭" />
-    <Container maxWidth="sm">
-    <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              minHeight: "10vh",
-            }}
-          ></Box>
-      <Box sx={{ width: "100%", typography: "body1" }}>
-      <Box sx={{ width: "100%", typography: "body1" }}></Box>
-      <Card>
-        <CardContent>
-          <Typography variant="h6">
-            送信者: {dm.sender}
-          </Typography>
-          <Typography variant="body1">
-            内容: {dm.content}
-          </Typography>
-          <Typography variant="body2" color="textSecondary">
-            タイムスタンプ: {dm.timestamp}
-          </Typography>
-        </CardContent>
-      </Card>
-      </Box>
-    </Container>
-    </div>
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            marginTop: "16px",
+            border: "1px solid #f8f8f8", // グレーの枠線を追加
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            minHeight: "90vh",
+            backgroundColor:"#f8f8f8"
+          }}
+        >
+          <ThemeProvider theme={theme}>
+          <List sx={{ width: "100%", padding: 2 }}>
+              {messages.map((message) => (
+                <ListItem
+                  key={message.id}
+                  sx={{
+                    alignSelf: message.sender === "田中" ? "flex-start" : "flex-end",
+                    marginBottom: 2,
+                  }}
+                >
+                  <Card
+                    sx={{
+                      backgroundColor: message.sender === "管理者" ? "#DDDDDD" : "#06C756",
+                      color: message.sender === "管理者" ? "#000000" :"#FFFFFF",
+                      width: "fit-content",
+                      boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+                      borderRadius: "12px",
+                      padding: 1,
+                    }}
+                  >
+                  <CardContent>
+                    <Typography variant="subtitle2" sx={{ marginBottom: 1 }}>
+                      {message.sender}
+                    </Typography>
+                    <Typography variant="body1">
+                      {message.content}
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      color="textSecondary"
+                      sx={{ alignSelf: "flex-end", marginTop: 1 }}
+                    >
+                      {message.timestamp}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </ListItem>
+            ))}
+          </List>
+          </ThemeProvider>
+        </Box>
+      </Container>
     </div>
   );
 };
 
 export default DmPanel;
+
+
+
+
--- src/Administrator/Dmdata.tsx
+++ src/Administrator/Dmdata.tsx
@@ -1,20 +1,29 @@
-// dmData.ts
+const dmData: any = {
+  1: {
+    id: 1,
+    sender: "田中",
+    content: "選手交代のお願い",
+    timestamp: "2023-08-06T12:00:00",
+  },
+  2: {
+    id: 2,
+    sender: "管理者",
+    content: "承知致しました。田中くんから誰に交代しますか",
+    timestamp: "2023-08-06T12:05:00",
+  },
+  3: {
+    id: 3,
+    sender: "田中",
+    content: "ありがとうございます。田中から吉田に変更お願いします",
+    timestamp: "2023-08-06T12:10:00",
+  },
+  4: {
+    id: 4,
+    sender: "管理者",
+    content: "承知致しました。変更します",
+    timestamp: "2023-08-06T12:15:00",
+  },
+  // Add more DM data here if needed
+};
 
-const dmData:any = {
-    1: {
-      id: 1,
-      sender: "田中",
-      content: "選手交代のお願い",
-  
-    },
-    2: {
-      id: 2,
-      sender: "User2",
-      content: "Hi User1! This is the second message.",
-      
-    },
-    // Add more DM data here if needed
-  };
-  
-  export default dmData;
-  
+export default dmData;
